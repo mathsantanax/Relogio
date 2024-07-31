742a1a262e89fb044bbc8e820d62e30be3e21219
Matheus Santana <matheus.santana7@hotmail.com>
2024-07-31 00:40:24 -0300

    tentando arrumar a pagina dinamica

--- Site/assets/js/Relogio.js
+++ Site/assets/js/Relogio.js
@@ -5,7 +5,8 @@ const Zones = {
     'Asia/Tokyo': 'ja-JP',
     'Australia/Sydney': 'en-US',
     'Europe/Athens': 'el-GR',
-    'Europe/Zurich': 'de-CH'
+    'Europe/Zurich': 'de-CH',
+    'America/Brasilia': 'pt-BR'
 };
 
 const imagensPaises = {
